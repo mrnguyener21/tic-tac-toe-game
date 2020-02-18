dbe56a1c0c48e0af763e852f152b07f81196c198
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-02-18 08:16:14 -0800

    add a way to switch between players(still need some work on it)

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const player = document.querySelector('.player');
 const theSquares = document.querySelector('.theSquares');
 const square = document.querySelectorAll('.square');
 
+const numberOfSquares = 9;
 //click on start game show player one
-player.addEventListener("click",()=>{ player.textContext = 'PLAYER ONE'});
+player.addEventListener("click",() => { player.textContext = 'PLAYER ONE'});
 //switch between player 1 and player 2. p.layer 1 = X and player 2 = 0. display which player is currently playing.
+if (player.textContent === 'PLAYER ONE'){
+    for (let i = 0; i < square; index++) {
+        square[i].addEventListener = ('click', () => {
+            square.textContent = 'X';
+            alert('PLAYER TWO\'S TURN');
+            player.textContent = 'PLAYER TWO';
+        });
+}else{
+    for (let i = 0; i < square; index++) {
+        square[i].addEventListener = ('click', () => {
+            square.textContent = 'X';
+            alert('PLAYER TWO\'S TURN');
+            player.textContent = 'PLAYER TWO';
+        });
+    };
 
 //for the squares if player 1 click textedit X if player 2 textedit O. if three squares match put a dash through them. will probably have to make three arrays signifying how the squares have to line up based on their number.
 
