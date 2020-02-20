3c1d16c5659d6b6beb2a5b94a27f765e063267b9
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-02-20 07:47:39 -0800

    added a reset function

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ const theSquares = document.querySelector('.theSquares');
 const square = document.querySelectorAll('.square');
 
 const numberOfSquares = 9;
-const player1Score = [];
-const player2Score =[];
+let player1Score = [];
+let player2Score =[];
+// possible winning arrays;
+
+const reset = () =>{
+    player.textContent = 'PLAY AGAIN?';
+    player.addEventListener('click', () =>{
+        square.textContent ='';
+        player.textContent = 'PLAYER ONE';
+    });
+};
 
 //click on start game show player one
 player.addEventListener('click', () => { player.textContent = 'PLAYER ONE' });
 
 for (let i = 0; i < square.length; i++) {
+    console.log(square[i]);
     square[i].addEventListener('click',() => {
         if (player.textContent === 'PLAYER ONE'){
             square[i].textContent = 'X';
+            alert(square[i]);
             //alert('PLAYER TWO\'S TURN');
             player.textContent = 'PLAYER TWO';
         } else if (player.textContent === 'PLAYER TWO'){
@@ -24,6 +35,8 @@ for (let i = 0; i < square.length; i++) {
     });
 };
 
-// need to determine who the winner is
+// need to determine who the winner is. create a for loop to push the clicked square into the respective playerscore.
 
-//need to reset the game
+
+
+
