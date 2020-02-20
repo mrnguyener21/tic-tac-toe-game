297700a7972afa2fcdc1fe831cdf1fbd3f892ad0
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-02-19 19:54:56 -0800

    added function to resolve start issue

--- script.js
+++ script.js
@@ -4,29 +4,33 @@ const theSquares = document.querySelector('.theSquares');
 const square = document.querySelectorAll('.square');
 
 const numberOfSquares = 9;
-const player1 = [];
-const player2 =[];
-//click on start game show player one
-player.addEventListener("click",() => { player.textContext = 'PLAYER ONE'});
-//switch between player 1 and player 2. p.layer 1 = X and player 2 = 0. display which player is currently playing.
-if (player.textContent === 'PLAYER ONE'){
-    for (let i = 0; i < square; index++) {
-        square[i].addEventListener = ('click', () => {
-            square.textContent = 'X';
-            alert('PLAYER TWO\'S TURN');
-            player.textContent = 'PLAYER TWO';
-        });
-    } 
-} else {
-    for (let i = 0; i < square; index++) {
-        square[i].addEventListener = ('click', () => {
-            square.textContent = 'O';
-            alert('PLAYER ONE\'S TURN');
-            player.textContent = 'PLAYER ONE';
-        });
+const player1Score = [];
+const player2Score =[];
+
+const startGame = () => {
+    //click on start game show player one
+    player.addEventListener("click",() => { player.textContext = 'PLAYER ONE'});
+    //switch between player 1 and player 2. p.layer 1 = X and player 2 = 0. display which player is currently playing.
+    if (player.textContent === 'PLAYER ONE'){
+        for (let i = 0; i < square; index++) {
+            square[i].addEventListener = ('click', () => {
+                square.textContent = 'X';
+                alert('PLAYER TWO\'S TURN');
+                player.textContent = 'PLAYER TWO';
+            });
+        } 
+    } else {
+        for (let i = 0; i < square; index++) {
+            square[i].addEventListener = ('click', () => {
+                square.textContent = 'O';
+                alert('PLAYER ONE\'S TURN');
+                player.textContent = 'PLAYER ONE';
+            });
+        };
     };
-};
-    
+        
+}
+
 //for the squares if player 1 click textedit X if player 2 textedit O. if three squares match put a dash through them. will probably have to make three arrays signifying how the squares have to line up based on their number.
 
 //base on which player got the winning array display that they won
