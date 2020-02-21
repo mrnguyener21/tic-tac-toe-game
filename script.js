294b086eb435c9ba261ceb5ec0af297125433c9a
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-02-21 08:11:48 -0800

    added a switch statement to both playerScore for the different cases of winning the game

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const player = document.querySelector('#player');
 const theSquares = document.querySelector('.theSquares');
 const square = document.querySelectorAll('.square');
 
-const numberOfSquares = 9;
 const numberedSquares =[];
 let player1Score = [];
 let player2Score =[];
-// possible winning arrays;
+
+
+for (let i = 0; i < square.length; i++) {numberedSquares.push(square[i]);};
 
 const reset = () =>{
     player.textContent = 'PLAY AGAIN?';
@@ -17,18 +18,14 @@ const reset = () =>{
     });
 };
 
-//click on start game show player one
+
 player.addEventListener('click', () => { player.textContent = 'PLAYER ONE' });
-
-for (let i = 0; i < square.length; i++) {numberedSquares.push(square[i]);};
-
+// push whatever is clicked into the player;s score array
 
 for (let i = 0; i < square.length; i++) {
-    console.log(square[i]);
     square[i].addEventListener('click',() => {
         if (player.textContent === 'PLAYER ONE'){
             square[i].textContent = 'X';
-            console.log(square[i]);
             //alert('PLAYER TWO\'S TURN');
             player.textContent = 'PLAYER TWO';
         } else if (player.textContent === 'PLAYER TWO'){
@@ -39,6 +36,72 @@ for (let i = 0; i < square.length; i++) {
     });
 };
 
+// possible winning arrays; multiple possibilities = multiple if this happen = switch statement
+/*
+the index positions for winning combinations
+0,1,2
+3,4,5
+6,7,8
+
+0,3,6
+1,4,7
+2,5,8
+
+0,4,8
+2,4,6
+*/
+switch(player1Score){
+    case [numberedSquares[0],numberedSquares[1],numberedSquares[2]]:
+
+    break;
+    case [numberedSquares[3],numberedSquares[4],numberedSquares[5]]:
+
+    break;
+    case [numberedSquares[6],numberedSquares[7],numberedSquares[8]]:
+
+    break;
+    case [numberedSquares[0],numberedSquares[3],numberedSquares[6]]:
+
+    break;
+    case [numberedSquares[1],numberedSquares[4],numberedSquares[7]]:
+
+    break;
+    case [numberedSquares[2],numberedSquares[5],numberedSquares[8]]:
+
+    break;
+    case [numberedSquares[0],numberedSquares[4],numberedSquares[8]]:
+
+    break;
+    case [numberedSquares[2],numberedSquares[4],numberedSquares[6]]:
+
+    break;
+}
+switch(player2Score){
+    case [numberedSquares[0],numberedSquares[1],numberedSquares[2]]:
+
+    break;
+    case [numberedSquares[3],numberedSquares[4],numberedSquares[5]]:
+
+    break;
+    case [numberedSquares[6],numberedSquares[7],numberedSquares[8]]:
+
+    break;
+    case [numberedSquares[0],numberedSquares[3],numberedSquares[6]]:
+
+    break;
+    case [numberedSquares[1],numberedSquares[4],numberedSquares[7]]:
+
+    break;
+    case [numberedSquares[2],numberedSquares[5],numberedSquares[8]]:
+
+    break;
+    case [numberedSquares[0],numberedSquares[4],numberedSquares[8]]:
+
+    break;
+    case [numberedSquares[2],numberedSquares[4],numberedSquares[6]]:
+
+    break;
+}
 // need to determine who the winner is. 
     //create a for loop to push the clicked square into the respective playerscore.
 
