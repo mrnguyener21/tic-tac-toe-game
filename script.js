6eff1ce60e7a9dcbae59c07af7629bf8fe8cd48c
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-02-22 12:58:23 -0800

    first attempt at connecting player scoreto winning combination along with additional brainstorming on what is required

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ const numberedSquares =[];
 ///probably a better way to go about this then hardcoding everything
 const winningCombination = [[numberedSquares[0],numberedSquares[1],numberedSquares[2]],[numberedSquares[3],numberedSquares[4],numberedSquares[5]],[numberedSquares[6],numberedSquares[7],numberedSquares[8]],[numberedSquares[0],numberedSquares[3],numberedSquares[6]], [numberedSquares[1],numberedSquares[4],numberedSquares[7]], [numberedSquares[2],numberedSquares[5],numberedSquares[8]], [numberedSquares[0],numberedSquares[4],numberedSquares[8]], [numberedSquares[2],numberedSquares[4],numberedSquares[6]]];
 
-let player1Score = [];
-let player2Score = [];
+let player1Score = [];// i have to be able to match what is inside here to numberedSquares' indexes. right now displays [div.square], needs to display [numberedSquares[1]] as an example
+let player2Score = [];// i have to be able to match what is inside here to numberedSquares' indexes
 
 for (let i = 0; i < squares.length; i++) {
     numberedSquares.push(squares[i]);
 };
+
 
 const reset = () => {
     player.textContent = 'PLAY AGAIN?';
@@ -32,7 +33,6 @@ for (let i = 0; i < squares.length; i++) {
             squares[i].textContent = 'X';
             player1Score.push(squares[i]);
             console.log(player1Score);
-            //alert('PLAYER TWO\'S TURN');
             player.textContent = 'PLAYER TWO';
         } else if (player.textContent === 'PLAYER TWO'){
             player.textContent = 'PLAYER ONE'
@@ -40,12 +40,19 @@ for (let i = 0; i < squares.length; i++) {
         };
     });
 };
-
+console.log(numberedSquares);
 //creating a function that signals the winner
+//possibly need to create a for loop to loop through numberedSquares in order to have the scores see if they match to any of the combinations
     //possibilities: player 1 wins, player 2 draws, draw
     //if player 1 = winningCombinations[i] then player 1 wins
-    //else if player 2 = winningCombination[i] then player 2 wins
-    //if all the squares have text in them then that's a draw
+    if(player1Score === numberedSquares[i]){
+        //pop up text content  = PLAYER 1 WINS
+        //when i click in the PLAY AGAIN BUTTON = reset();
+    } else if (player2Score === numberedSquares[i]){   //else if player 2 = winningCombination[i] then player 2 wins
+           //pop up text content  = PLAYER 2 WINS
+        //when i click in the PLAY AGAIN BUTTON = reset();
+    } 
+    //if all the squares have text in them then that's a draw. 
     //figure out a function to end the game if any of the above three statements are executed
     //create a function for a pop up screen to appear
 
