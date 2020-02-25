cd7bec51195abd826ec96991ac92ef5cc1f32dce
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-02-25 07:31:32 -0800

    first attempt at determining a draw

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const numberedSquares =[];
 // 0,1,2 
 const winningCombination = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
-let player1Score = [];// i have to be able to match what is inside here to numberedSquares' indexes. right now displays [div.square], needs to display [numberedSquares[1]] as an example
-let player2Score = [];// i have to be able to match what is inside here to numberedSquares' indexes
+let player1Score = [];
+let player2Score = [];
 
 for (let i = 0; i < squares.length; i++){
     numberedSquares.push(squares[i]);
@@ -24,8 +24,7 @@ const reset = () => {
 };
 
 player.addEventListener('click', () => player.textContent = 'PLAYER ONE');
-// push whatever is clicked into the player;s score array
-//figure out a way to prevent the player from overwriting a square that already has text in it
+
 
 for (let i = 0; i < squares.length; i++) {
 
@@ -41,9 +40,6 @@ for (let i = 0; i < squares.length; i++) {
             player.textContent = 'PLAYER ONE'
             squares[i].textContent = 'O'; 
         };
-        // if(player1Score.some( () => [...winningCombination[i]] )){
-        //     console.log('this if else statement works');
-        // }
 
         //i can probably be more concise by using the same concept but looping through winning combinations and call the array within the array. so winningcombo[i][0] ===so winningcombo[i][1] &&winningcombo[i][1] === winningcombo[i][2]
         //WINNING COMBO FOR PLAYER 1 
@@ -70,9 +66,8 @@ for (let i = 0; i < squares.length; i++) {
         }
         else if(numberedSquares[2].textContent  === 'X' && numberedSquares[4].textContent  === 'X' &&numberedSquares[6].textContent  === 'X'){
             console.log( 'it works')
-        };
-        // WINNING COMBO FOR PLAYER 2
-        if (numberedSquares[0].textContent  === 'O' && numberedSquares[1].textContent  === 'O' &&numberedSquares[2].textContent  === 'O'){
+
+        }else if (numberedSquares[0].textContent  === 'O' && numberedSquares[1].textContent  === 'O' &&numberedSquares[2].textContent  === 'O'){
             console.log( 'it works');//why doesn't it work if i don't use textcontent???
         } 
         else if(numberedSquares[3].textContent  === 'O' && numberedSquares[4].textContent  === 'O' &&numberedSquares[5].textContent  === 'O'){
@@ -95,12 +90,20 @@ for (let i = 0; i < squares.length; i++) {
         }
         else if(numberedSquares[2].textContent  === 'O' && numberedSquares[4].textContent  === 'O' &&numberedSquares[6].textContent  === 'O'){
             console.log( 'it works')
+        }
+        else {
+            console.log ('draw')
         };
-
     });
 };
+//CREATE A FUNCTION TO DETERMINE A DRAW
+// a draw means that all the squares has a string contained in it, but none of them line up
 
+//CREATE A FUNCTION SO THE GAME CAN'T CONTINUE IF A WINNER OR DRAW IS DETERMINED
 
+//CREATE A FUNCTION SO IF THE SQUARE CONTAINS A TEXT THE OTHER PLAYER CAN'T OVERWRITE IT
+
+//CREATE A FUNCTION SO WHEN ITS'S PLAYER 2 WE CAN'T CLICK ON THE BUTTON TO CHANGE TO PLAYER 1 CAUSE THAT'S HAPENING FOR SOME REASON
 
 
 
