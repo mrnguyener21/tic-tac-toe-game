57fa87520f55bdc651084b8bb4f38b53c550a65d
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-02-26 08:06:08 -0800

    basic tic tac toe project completed, will continue adding improvements onto project

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let player2Score = [];
 for (let i = 0; i < squares.length; i++){
     numberedSquares.push(squares[i]);
 } 
-
+//this will be for later when the pop up is created
 const reset = () => {
     player.textContent = 'PLAY AGAIN?';
     player.addEventListener('click', () => {
@@ -47,68 +47,83 @@ for (let i = 0; i < squares.length; i++) {
         } 
         else if(numberedSquares[3].textContent  === 'X' && numberedSquares[4].textContent  === 'X' &&numberedSquares[5].textContent  === 'X'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[6].textContent  === 'X' && numberedSquares[7].textContent  === 'X' &&numberedSquares[8].textContent  === 'X'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[0].textContent  === 'X' && numberedSquares[3].textContent  === 'X' &&numberedSquares[6].textContent  === 'X'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[1].textContent  === 'X' && numberedSquares[4].textContent  === 'X' &&numberedSquares[7].textContent  === 'X'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[2].textContent  === 'X' && numberedSquares[5].textContent  === 'X' &&numberedSquares[8].textContent  === 'X'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[0].textContent  === 'X' && numberedSquares[4].textContent  === 'X' &&numberedSquares[8].textContent  === 'X'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[2].textContent  === 'X' && numberedSquares[4].textContent  === 'X' &&numberedSquares[6].textContent  === 'X'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         
         }else if (numberedSquares[0].textContent  === 'O' && numberedSquares[1].textContent  === 'O' &&numberedSquares[2].textContent  === 'O'){
             console.log( 'it works');//why doesn't it work if i don't use textcontent???
+            player.textContent = 'PLAY AGAIN?'
         } 
         else if(numberedSquares[3].textContent  === 'O' && numberedSquares[4].textContent  === 'O' &&numberedSquares[5].textContent  === 'O'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[6].textContent  === 'O' && numberedSquares[7].textContent  === 'O' &&numberedSquares[8].textContent  === 'O'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[0].textContent  === 'O' && numberedSquares[3].textContent  === 'O' &&numberedSquares[6].textContent  === 'O'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[1].textContent  === 'O' && numberedSquares[4].textContent  === 'O' &&numberedSquares[7].textContent  === 'O'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[2].textContent  === 'O' && numberedSquares[5].textContent  === 'O' &&numberedSquares[8].textContent  === 'O'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[0].textContent  === 'O' && numberedSquares[4].textContent  === 'O' &&numberedSquares[8].textContent  === 'O'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if(numberedSquares[2].textContent  === 'O' && numberedSquares[4].textContent  === 'O' &&numberedSquares[6].textContent  === 'O'){
             console.log( 'it works')
+            player.textContent = 'PLAY AGAIN?'
         }
         else if ((numberedSquares.every((numberedSquares) => numberedSquares.textContent === 'X'||numberedSquares.textContent === 'O' ))){
         console.log('draw');// why do i need a parameter
+        player.textContent = 'PLAY AGAIN?'
         };
 
 
         if(player.textContent === 'PLAY AGAIN?'){
             player.addEventListener('click', () => {
                 numberedSquares.textContent = '';
+                squares.forEach ((numberedSquares) => numberedSquares.textContent = '')
                 player.textContent = 'PLAYER ONE';
             });
         }
     });
 };
 
-//CREATE A FUNCTION SO THE GAME CAN'T CONTINUE IF A WINNER OR DRAW IS DETERMINED
 
-//CREATE A FUNCTION SO IF THE SQUARE CONTAINS A TEXT THE OTHER PLAYER CAN'T OVERWRITE IT
 
-//CREATE A FUNCTION SO WHEN ITS'S PLAYER 2 WE CAN'T CLICK ON THE BUTTON TO CHANGE TO PLAYER 1 CAUSE THAT'S HAPENING FOR SOME REASON
+
 
 
 
