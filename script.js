ae8dd77417840c2e62fbe7740672ca9ed2285b66
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-02-29 20:39:17 -0800

    fixed counter issue

--- script.js
+++ script.js
@@ -9,38 +9,49 @@ const winningCombinations = [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ], [ 0, 3, 6 ]
 let activePlayer = 1;
 let player1Score = 0;
 let player2Score = 0;
+let winner = null;
 
-score.textContent = `${player1Score}:${player2Score}`;
-
-const getWinner = () => {
-    let winner = null;
-    
-    winningCombinations.forEach((winningCombination) => {
-        if (squares[winningCombination[0]].textContent && squares[winningCombination[0]].textContent === squares[winningCombination[1]].textContent && squares[winningCombination[0]].textContent === squares[winningCombination[2]].textContent) {
-            winner = squares[winningCombination[0]].textContent;
-        };
-    });
-
-    if(winner === 'X') {
-        player1Score++;
-        reset();
-    } else if(winner === 'O') {
-        player2Score++;
-        reset();
-    } 
-
-    // reset if there's a draw
-    // have alert display correct winner
-    // finish creating the counter function
-}
+updatingTheScore = () => score.textContent = `${player1Score}:${player2Score}`;
+updatingTheScore();
 
 const reset = () => {
-    alert(`Player ${activePlayer} wins!`);
-
+    // alert(`Player ${activePlayer} wins!`);
     for(let i = 0; i < 9; i++){
         squares[i].textContent = '';
     }
+    winner = null;
 }; 
+
+
+const getWinner = () => {
+   
+    winningCombinations.forEach((winningCombination) => {
+        if (squares[winningCombination[0]].textContent && squares[winningCombination[0]].textContent === squares[winningCombination[1]].textContent &&squares[winningCombination[0]].textContent === squares[winningCombination[2]].textContent) {
+            winner = squares[winningCombination[0]].textContent;
+        };
+    });
+    if(winner === 'X') {
+        ++player1Score;
+        console.log(winner);
+        console.log(player1Score);
+        console.log(score);
+        updatingTheScore();
+        reset();
+    } else if(winner === 'O') {
+        ++player2Score;
+        updatingTheScore();
+        reset();
+    } 
+    
+
+
+
+    // reset if there's a draw
+    //find a way to change the winner variable into X, O, or T(for tie)
+    // reset winner variable afterwards
+    // have alert display correct winner
+    // finish creating the counter function
+}
 
 for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', () => {
@@ -53,7 +64,8 @@ for (let i = 0; i < squares.length; i++) {
             heading.textContent = 'PLAYER TWO';
             activePlayer = 1;
         };     
-    
+        console.log(winner)
+
         getWinner();
     });
 };
